588b1151f8589e540e34e7c83af176c30d9bbebc
AndyRafael <andyrafaelmar@gmail.com>
2022-08-17 22:46:56 -0400

    Propiedades v1

---
+++ RealStateApp.Core.Application/Services/PropertyService.cs
@@ -0,0 +1,32 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using RealStateApp.Core.Application.Helpers;
+using RealStateApp.Core.Application.Interfaces.Repositories;
+using RealStateApp.Core.Application.Interfaces.Services;
+using RealStateApp.Core.Application.ViewModels.Properties;
+using RealStateApp.Core.Application.ViewModels.PropertyType;
+using RealStateApp.Core.Application.ViewModels.User;
+using RealStateApp.Core.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RealStateApp.Core.Application.Services
+{
+    public class PropertyService : GenericService<SavePropertyViewModel, PropertyViewModel,Property>, IPropertyService
+    {
+        private readonly IPropertyRepository _propertyRepository;
+        private readonly IMapper _mapper;
+        private readonly IHttpContextAccessor _httpContextAccessor;
+        private readonly UserViewModel userViewModel;
+        public PropertyService(IPropertyRepository propertyRepository, IMapper mapper, IHttpContextAccessor httpContextAccessor) : base(propertyRepository , mapper)
+        {
+            _propertyRepository = propertyRepository;
+            _mapper = mapper;
+            _httpContextAccessor = httpContextAccessor;
+            userViewModel = _httpContextAccessor.HttpContext.Session.Get<UserViewModel>("user");
+        }
+    }
+}
