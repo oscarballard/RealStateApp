662f330e8e48dbd54edbc0658fc799e0092946de
AndyRafael <andyrafaelmar@gmail.com>
2022-08-19 20:26:40 -0400

    form properties

---
+++ WebApp.RealStateApp/Controllers/PropertiesController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RealStateApp.Core.Application.Interfaces.Services;
+using RealStateApp.Core.Application.ViewModels.Properties;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApp.RealStateApp.Controllers
+{
+    public class PropertiesController : Controller
+    {
+        private readonly IPropertyTypeService _propertyTypeService;
+        private readonly ISalesTypeService _salesTypeService;
+        private readonly IImprovementsService _improvementsService;
+        public PropertiesController(ISalesTypeService salesTypeService, IPropertyTypeService propertyTypeService, IImprovementsService improvementsService)
+        {
+            _propertyTypeService = propertyTypeService;
+            _salesTypeService = salesTypeService;
+            _improvementsService = improvementsService;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public async Task<IActionResult> Create()
+        {
+            ViewBag.PropertyType = await _propertyTypeService.GetAllViewModel();
+            ViewBag.SalesType = await _salesTypeService.GetAllViewModel();
+            ViewBag.Mejoras = await _improvementsService.GetAllViewModel();
+
+            return View("SaveProperty",new SavePropertyViewModel());
+        }
+    }
+}
