e35431f3dbc33646fb32805a5bd9e18b876efabd
AndyRafael <andyrafaelmar@gmail.com>
2022-08-27 12:48:13 -0400

    controladore de tipo de propiedades

---
+++ RealStateApp.Core.Application/Features/PropertyType/Commands/CreatePropertyType/CreatePropertyTypeCommand.cs
@@ -0,0 +1,38 @@
+﻿using AutoMapper;
+using MediatR;
+using RealStateApp.Core.Application.Interfaces.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace RealStateApp.Core.Application.Features.PropertyType.Commands.CreatePropertyType
+{
+    public class CreatePropertyTypeCommand : IRequest<int>
+    {
+        public string Nombre { get; set; }
+        public string Descripcion { get; set; }
+    }
+
+    public class CreatePropertyTypeCommandHandler : IRequestHandler<CreatePropertyTypeCommand, int>
+    {
+        private readonly IPropertyTypeRepository _propertyTypeRepository;
+        private readonly IMapper _mapper;
+        public CreatePropertyTypeCommandHandler(IPropertyTypeRepository propertyTypeRepository, IMapper mapper)
+        {
+            _propertyTypeRepository = propertyTypeRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<int> Handle(CreatePropertyTypeCommand request, CancellationToken cancellationToken)
+        {
+            var propertyType = _mapper.Map<Domain.Entities.PropertyType>(request);
+
+            var response = await _propertyTypeRepository.AddAsync(propertyType);
+
+            return response.Id;
+        }
+    }
+}
---
+++ RealStateApp.Core.Application/Features/PropertyType/Commands/DeletePropertyTypeById/DeletePropertyTypeByIdCommand.cs
@@ -0,0 +1,39 @@
+﻿using AutoMapper;
+using MediatR;
+using RealStateApp.Core.Application.Interfaces.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace RealStateApp.Core.Application.Features.PropertyType.Commands.DeletePropertyTypeById
+{
+    public class DeletePropertyTypeByIdCommand : IRequest<int>
+    {
+        public int Id { get; set; }
+    }
+
+    public class DeletePropertyTypeByIdCommandHandler : IRequestHandler<DeletePropertyTypeByIdCommand, int>
+    {
+        private readonly IPropertyTypeRepository _propertyTypeRepository;
+        private readonly IMapper _mapper;
+        public DeletePropertyTypeByIdCommandHandler(IPropertyTypeRepository propertyTypeRepository, IMapper mapper)
+        {
+            _propertyTypeRepository = propertyTypeRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<int> Handle(DeletePropertyTypeByIdCommand request, CancellationToken cancellationToken)
+        {
+            var propertyType = await _propertyTypeRepository.GetByIdAsync(request.Id);
+
+            if (propertyType == null) throw new Exception("No Existe este Id");
+
+            await _propertyTypeRepository.DeleteAsync(propertyType);
+
+            return propertyType.Id;
+        }
+    }
+}
